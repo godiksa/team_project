801970f166caffc2a3f61daa1488b4449a6f0511
fbielske <f.gedaminskaite@gmail.com>
2023-06-30 10:52:30 +0300

    input atom created

--- src/components/atoms/Input/styles.ts
+++ src/components/atoms/Input/styles.ts
@@ -1,1 +1,38 @@
 import { styled } from 'styled-components';
+
+export const StyledInputWrapper = styled.div`
+  width: 95%;
+  border-radius: 0.375em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  outline: none;
+  border: 1px solid transparent;
+  border-color: ${(props) => props.theme.palette.shades.greyLight};
+  background-color: ${(props) => props.theme.palette.white};
+  height: 35px;
+  margin: 15px;
+  max-width: 1200px;
+`;
+
+export const StyledIcon = styled.span`
+  opacity: 1;
+  height: 14px;
+  width: 14px;
+  padding-left: 15px;
+`;
+export const StyledInput = styled.input`
+  outline: none;
+  border: none;
+  background-color: ${(props) => props.theme.palette.white};
+  border-radius: 0.375em;
+  background-color: ${(props) => props.theme.palette.white};
+
+  width: 100%;
+  color: #676c7e;
+  padding: 0 13px;
+
+  &::placeholder {
+    color: #9b9eac;
+  }
+`;
