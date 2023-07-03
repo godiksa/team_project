716628df2025e6fac172cccbd047119b91b59c5b
antropofilija <alininyte1@gmail.com>
2023-07-03 10:04:36 +0300

    Fixed Select atom

--- src/pages/Individualios_veiklos_mokesčių_skaičiuoklė/Individualios_veiklos_mokesčių_skaičiuoklė.tsx
+++ src/pages/Individualios_veiklos_mokesčių_skaičiuoklė/Individualios_veiklos_mokesčių_skaičiuoklė.tsx
@@ -1,5 +1,3 @@
-import Select from '../../components/atoms/Select';
-
 const Individualios_veiklos_mokesčių_skaičiuoklė = () => {
   return <main>Individualios_veiklos_mokesčių_skaičiuoklė</main>;
 };
