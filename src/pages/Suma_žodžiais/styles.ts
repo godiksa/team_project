638c7438b2654b79f84c3dc2e5577f9fdcb3e269
Goda <goda.vaicyte@gmail.com>
2023-07-06 21:20:15 +0300

    created Suma_zodziais page

---
+++ src/pages/Suma_žodžiais/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const StyledDisplay = styled.div`
+  div {
+    margin: auto;
+    max-width: 1000px;
+    padding: 8px 24px;
+  }
+
+  div div {
+    width: 100%;
+    justify-content: center;
+    margin-top: 5px;
+    background-color: ${(props) => props.theme.palette.primary.main};
+    border-radius: 0.375em;
+    margin-top: 20px;
+  }
+
+  p {
+    color: ${(props) => props.theme.palette.white};
+    font-weight: 400;
+    line-height: 1.5;
+  }
+`;
+
+export const StyledFormWrapper = styled.div`
+  div {
+    width: 100%;
+    margin: auto;
+    max-width: 1000px;
+  }
+
+  input {
+    color: ${(props) => props.theme.palette.shades.greyDarker};
+    font-weight: 400;
+    width: 100%;
+  }
+`;
+
+export const StyledTitle = styled.h2`
+  text-align: center;
+  font-weight: 600;
+  font-size: 24px;
+  color: ${(props) => props.theme.palette.shades.greyDarker};
+  margin-top: 44px;
+`;
